17fc1d0b9f32da33179bb06427c220cae15b7d2e
Sikver29 <maximschiaroli@gmail.com>
2025-11-20 16:21:54 +0300

    Update Тз3.py
    Добавлена обработка ошибок

--- task3/Тз3.py
+++ task3/Тз3.py
@@ -3,13 +3,24 @@ import json
 
 #загрузка json из файла
 def load_json(filepath):
-    with open(filepath) as f:
-        return json.load(f)
+    try:
+        with open(filepath) as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print("Error: файл не найден")
+        sys.exit(1)
+    except OSError:
+        print("Error: не удалось открыть файл")
+        sys.exit(1)
 
 #сохранение данных в json
 def save_json(filepath, data):
-    with open(filepath, 'w') as f:
-        json.dump(data, f, indent=2)
+    try:
+        with open(filepath, 'w') as f:
+            json.dump(data, f, indent=2)
+    except OSError:
+        print("Ошибка: не удалось записать файл")
+        sys.exit(1)
 
 #Поиск по Id
 def create_values_dict(values_data):
@@ -19,10 +30,14 @@ def create_values_dict(values_data):
         for item in values_data["values"]:
             test_id = item["id"]
             test_value = item["value"]
+
+            if test_value == test_id:
+                print(f"Error: в values.json значение value совпадает с id ({test_id})")
+                print("Ожидается что 'value' хранит результат теста а не номер")
+                sys.exit(1)
+
             values_dict[test_id] = test_value
-
     return values_dict
-
 
 # Заполнение value
 def fill_tests(tests_list, values_dict):
@@ -38,6 +53,7 @@ def fill_tests(tests_list, values_dict):
 
 def main():
     if len(sys.argv) != 4:
+        print("Error: неверное количество аргументов")
         sys.exit(1)
 
     values_file = sys.argv[1]
@@ -47,6 +63,14 @@ def main():
     values_data = load_json(values_file)
     tests_data = load_json(tests_file)
 
+    if "values" not in values_data:
+        print("Error: первый файл должен быть values.json")
+        sys.exit(1)
+
+    if "tests" not in tests_data:
+        print("Error: второй файл должен быть tests.json")
+        sys.exit(1)
+
     #Словарь
     values_dict = create_values_dict(values_data)
 
@@ -55,7 +79,6 @@ def main():
         fill_tests(tests_data["tests"], values_dict)
 
     save_json(report_file, tests_data)
-
 
 if __name__ == "__main__":
     main()
