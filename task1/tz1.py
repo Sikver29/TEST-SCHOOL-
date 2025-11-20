033fc7123cb5abf825069419516981d2406a2de9
Sikver29 <maximschiaroli@gmail.com>
2025-11-20 16:07:54 +0300

    Update tz1.py
    Добавлена обработка ошибок ввода аргументов командной строки.
    Добавлены проверки корректности значений n и m для каждого массива
    Реализован ввод через аргументы командной строки

--- task1/tz1.py
+++ task1/tz1.py
@@ -1,4 +1,4 @@
-n1, m1, n2, m2 = map(int, input().split())
+import sys
 
 def circular_path(n, m):
     msv = [i + 1 for i in range(n)]
@@ -12,7 +12,37 @@ def circular_path(n, m):
             break
     return ''.join(str(x) for x in path)
 
-path1 = circular_path(n1, m1)
-path2 = circular_path(n2, m2)
+def main():
+    if len(sys.argv) != 5:
+        print("Error: Передайте 4 аргумента")
+        sys.exit(1)
 
-print(path1 + path2)
+    try:
+        n1, m1, n2, m2 = map(int, sys.argv[1:])
+
+    except ValueError:
+        print("Error: Аргументы должны быть целыми числами")
+        sys.exit(1)
+
+    if n1 <= 0 or m1 <= 0:
+        print("Error: n и m для массива 1 должны быть положительными числами")
+        sys.exit(1)
+
+    if m1 > n1:
+        print("Error: m не может быть больше n (массив 1)")
+        sys.exit(1)
+
+    if n2 <= 0 or m2 <= 0:
+        print("Error: n и m для массива 2 должны быть положительными числами")
+        sys.exit(1)
+
+    if m2 > n2:
+        print("Error: m не может быть больше n (массив 2)")
+        sys.exit(1)
+
+    path1 = circular_path(n1, m1)
+    path2 = circular_path(n2, m2)
+    print(path1 + path2)
+
+if __name__ == "__main__":
+    main()
